6460a606f06b740546a24c2a14753445cddacadb
Zhuoer Peter Zhou <ppz4725@gotham.ece.northwestern.edu>
2022-01-25 14:10:23 -0600

    lab04 stuff

---
+++ lab/lab04/src/broken.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "lab4_funs.h"
+
+
+int main(void)
+{
+    // Array of ten lab scores
+    // Note: sum should be 45 (calculated manually)
+    unsigned int lab_scores[10] = {1, 2, 3, 7, 6, 5, 0, 4, 9, 8};
+    printf("Sum of scores is: %d\n", sum_array(lab_scores, 10));
+
+    return 0;
+}
+
